b3e1b1ed75d5edc1af49fc8248c1d766e751aa84
Dashi <dbrkani@gmail.com>
2024-12-15 16:41:53 -0500

    flask to render ports

--- app.py
+++ app.py
@@ -146,4 +146,5 @@ def easteregg():
 
 # ======================= DEBUG ==========================
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
